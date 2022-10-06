1e08d0ae2e71a1940255e4ef4dc13692cce23bea
Kenan <haciyevk785@gmail.com>
2022-10-06 17:45:12 +0400

    Classes

--- js/script.js
+++ js/script.js
@@ -50,121 +50,116 @@ window.addEventListener('DOMContentLoaded', () => {
     //     }
     // });
 
+        
+    let view = [];
+    let id = 0;
+    class Weather {
+        constructor(date, temp) {
+            this.date = date;
+            this.id = id;
+            this.temp = temp;
+            this.obj = {
+                id: this.id,
+                date: this.dateBinding(),
+                day: this.dayBinding(),
+                temp: this.temp,
 
+            };
+            view.push(this.obj);
+            id++;
+        }          
+        
+        dateBinding () {
+            return new Date(this.date).toLocaleDateString();
+        }
+        
+        dayBinding() {
+            let dateNum = new Date(this.date);
+                let obj =  {
+                    weekDay: (() => {
+                        let dayId = dateNum.getDay();
+                        let day = new Map();
+                        day.set(0,'Воскресение');
+                        day.set(1,'Понедельник');
+                        day.set(2,'Вторник');
+                        day.set(3,'Среда');
+                        day.set(4,'Четверг');
+                        day.set(5,'Пятница');
+                        day.set(6,'Суббота');
+                        return day.get(dayId);
+                    })(),
+                };
+            return obj.weekDay;
+        }
 
-    //HOMEWORK OBJECTS______________________________________________________________________________________
-    
-    //1)
+        deleteDay() {
+            return view = view.filter(item => item.id !== this.id);
+        }
 
-    let i = 0;
-    const weather = [
-        {
-            id: i++,
-            weekDay: 'Четверг',
-            date: '2022-09-29',
-            temp: '+12'
-        },
-    ];
-
-    
-    //2)
-
-    function newDay(date, temperature) {
-        let dateNum = new Date(date);
-        let obj =  {
-            id: i++,
-            weekDay: (() => {
-                let dayId = dateNum.getDay();
-                let day;
-                switch (dayId) {
-                    case 0:
-                        day = 'Воскресение';
-                        break;
-                    case 1:
-                        day = 'Понедельник';
-                        break;
-                    case 2:
-                        day = 'Вторник';
-                        break;
-                    case 3:
-                        day = 'Среда';
-                        break;
-                    case 4:
-                        day = 'Четверг';
-                        break;
-                    case 5:
-                        day = 'Пятница';
-                        break;
-                    case 6:
-                        day = 'Суббота';
-                        break;
-                }
-                return day;
-            })(),
-            date: dateNum.toISOString().slice(0,10),
-            temp: temperature
-        };
-        return weather.push(obj);
-    }
-
-    newDay('2022-09-30', '+14');
-    newDay('2024-02-10', '+4');
-    newDay('2002-11-03', '-2');
-    console.log(weather);
-
-
-    //3)
-
-    function deleteDay(id) {
-        for (let key in weather) {
-            if (weather[key].id==id){
-                weather.splice(id,1);
-                weather.map(item => {
-                    if (item.id>id) {
-                        item.id--;      //Изменение iD элементов массива (обЪектов) после удаления элемента массива
-                    }
-                });
-                return weather;
+        sortingbyTemp(a=1) {
+            let res;
+            if (a===0) {
+                res =  view.sort((a,b) => +a.temp < +b.temp ? 1 : -1);
+            }else {
+                res =  view.sort((a,b) => +a.temp > +b.temp ? 1 : -1);
             }
+            return res;
+        }
+
+        copyList(a='weekdays') {
+            let res;
+            if (a === 'weekend') {
+                res = view.filter(item => item.day ==='Суббота' || item.day ==='Воскресение');
+            }else {
+                res = view.filter(item => item.day !=='Суббота' && item.day !=='Воскресение');
+            }
+            return console.log(res);
         }
     }
-    console.log(deleteDay(1));
 
+        const day1 = new Weather('2011-06-02', '-12');
+        const day2 = new Weather('2011-06-03', '-15');
+        const day3 = new Weather('2011-06-04', '-11');
+        const day4 = new Weather('2011-06-05', '-18');
+        const day5 = new Weather('2011-06-06', '+26');
+        const day6 = new Weather('2011-06-07', '+4');
+        const day7 = new Weather('2011-06-08', '+31');
     
-    //4)
+    console.log(view);  // Полный список
 
-    function sortingbyTemp(a=1) {
-        let res;
-        if (a==0) {
-            res =  weather.sort((a,b) => +a.temp < +b.temp ? 1 : -1);
-        }else {
-            res =  weather.sort((a,b) => +a.temp > +b.temp ? 1 : -1);
-        }
 
-        i=0;
-        weather.map(item =>item.id = i++);    // Установление новых id в связи с переменой последовательности элементов после сортировки
-        return res;
-    }
+    day3.deleteDay();   //Вызов метода deleteDay() того объекта класса, который вы хотите удалить
+    console.log(view);
+
 
     //для сортировки от большей temp к меньшей введите аргумент 0
-    sortingbyTemp();
-    console.log(weather);
+    day2.sortingbyTemp(); // Сортировка по температуре
+    console.log(view);
 
 
-    //5)
-
-    function copyList(a='weekdays') {
-        let res;
-        if (a == 'weekend') {
-            res = weather.filter(item => item.weekDay=='Суббота' || item.weekDay=='Воскресение');
-        }else {
-            res = weather.filter(item => item.weekDay =='Четверг' || item.weekDay =='Пятница');
-        }
-        i = 0;
-        res.map(item => item.id = i++);
-        return res;
-    }
     //для выведения копии списка выходных введите аргумент "weekend"
-    console.log(copyList(/* 'weekend' */));
+    day3.copyList();
+
+
+    class WeatherToday extends Weather{
+        constructor (date, temp) {
+            super(date, temp);
+        }
+
+        dateBinding() {
+            let dateNum = new Date(this.date);
+            return dateNum.toDateString();         //вывод даты в другом формате
+        }
+    }
+
+    let today = new WeatherToday( '2021-08-12','+13','http://path' );
+    let today2 = new WeatherToday( '2021-08-19','+15','http://path' );
+
+    console.log(view);
 
 });
+
+
+
+
+
